1f9853af6adf202b72b87a2d8228959881c39a89
vacantClaudia <vacantclaudia88@gmail.com>
2022-01-17 15:23:49 +0100

    intégration statique du formulaire

--- js/app.js
+++ js/app.js
@@ -3,8 +3,24 @@
  */
 var app = {
   init: function() {
-
+    app.creatForm();
   },
+
+  // formulaire
+  creatForm: function(){
+    const container = document.getElementById('todo');
+
+    const form = document.createElement('form');
+    form.className = 'form-addTask';
+
+    const input = document.createElement('input');
+    input.className = 'input-addTask';
+    input.placeholder = 'Ajouter une tâche';
+    input.type = 'text';
+
+    form.appendChild(input);
+    container.appendChild(form);
+  }
 };
 
 
